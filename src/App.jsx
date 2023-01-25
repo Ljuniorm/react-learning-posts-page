0bade5585297f517c22ee3dd92cb40beb9ccc02b
Luiz Junior <luiz.junior@blueticket.com.br>
2023-01-25 20:11:58 -0300

    project

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import { Header } from "./components/Header";
+import { Post } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
+
+import styles from "./App.module.css";
+import "./global.css";
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/Ljuniorm.png",
+      name: "Luiz Roberto",
+      role: "Full-stack Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋 " },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2023-01-19 20:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/Ljuniorm.png",
+      name: "Junior",
+      role: "Mobile Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋 " },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2023-01-15 20:00:00"),
+  },
+];
+
+function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          {posts.map((p) => (
+            <Post
+              key={p.id}
+              author={p.author}
+              content={p.content}
+              publishedAt={p.publishedAt}
+            />
+          ))}
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Comment.jsx
@@ -0,0 +1,37 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import { Avatar } from "./Avatar";
+import styles from "./Comment.module.css";
+
+export function Comment({ content }) {
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/Ljuniorm.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Luiz Roberto</strong>
+
+              <time title="11 de Maio às 8:13" dateTime="2022-05-11 08:13:30">
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp /> Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
